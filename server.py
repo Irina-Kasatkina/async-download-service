1cb0904abfe7308ea6a9d401f53bb6e26f080821
Irina Kasatkina <kasatkina235@gmail.com>
2022-12-15 19:40:29 +0300

    Add command line options

--- server.py
+++ server.py
@@ -1,20 +1,40 @@
 # coding=utf-8
 
-import aiofiles
+"""Asynchronous microservice for downloading files"""
+
+import argparse
 import asyncio
 import logging
 import os
+
+import aiofiles
 from aiohttp import web
 
 
-logging.basicConfig(level = logging.DEBUG)
-
 CHUNK_SIZE = 102400
+PHOTOS_FOLDER = 'test_photos'
+
+
+def create_parser():
+    """Create command line parameter parser."""
+
+    parser = argparse.ArgumentParser(description='Асинхронный микросервис для загрузки файлов.')
+    parser.add_argument('-l', '--logging', action='store_true', help='включить логирование')
+    parser.add_argument('-d', '--delay', action='store_true', help='включить задержку ответа')
+    parser.add_argument(
+        '-p',
+        '--path',
+        default=os.path.join(os.getcwd(), PHOTOS_FOLDER),
+        help='путь к каталогу с фотографиями'
+    )
+    return parser
 
 
 async def archive(request):
+    """Archive requested folder and send zip to user."""
+
     archive_hash = request.match_info.get('archive_hash', '')
-    archive_path = os.path.join(os.getcwd(), 'test_photos', archive_hash)
+    archive_path = os.path.join(request.app.args.path, archive_hash)
     if not archive_hash or not os.path.exists(archive_path):
         raise web.HTTPNotFound(text='Архив не существует или был удален')
 
@@ -24,7 +44,7 @@ async def archive(request):
     await response.prepare(request)
 
     args = ['-qr', '-', '.']
-    process: Process = await asyncio.create_subprocess_exec(
+    process = await asyncio.create_subprocess_exec(
         'zip',
         *args,
         cwd=archive_path,
@@ -35,11 +55,14 @@ async def archive(request):
     try:
         while not process.stdout.at_eof():
             zip_chunk = await process.stdout.read(CHUNK_SIZE)
-            logging.debug('Sending archive chunk ...')
+            if request.app.args.logging:
+                logging.debug('Sending archive chunk ...')
             await response.write(zip_chunk)
-            await asyncio.sleep(1)
+            if request.app.args.delay:
+                await asyncio.sleep(1)
     except asyncio.CancelledError:
-        logging.debug('Download was interrupted')
+        if request.app.args.logging:
+            logging.debug('Download was interrupted')
         process.kill()
         await process.communicate()
         raise
@@ -47,15 +70,20 @@ async def archive(request):
         return response
 
 
-
 async def handle_index_page(request):
+    """Handle index page."""
+
     async with aiofiles.open('index.html', mode='r') as index_file:
         index_contents = await index_file.read()
+
     return web.Response(text=index_contents, content_type='text/html')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
     app = web.Application()
+    app.args = create_parser().parse_args()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
